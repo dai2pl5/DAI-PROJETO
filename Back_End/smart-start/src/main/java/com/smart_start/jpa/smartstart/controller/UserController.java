8ce2edf4a646f7e01966cc011e19ca416798a584
smartstartgsi <48456976+smartstartgsi@users.noreply.github.com>
2019-03-22 00:34:07 +0000

    Add files via upload

--- Back_End/smart-start/src/main/java/com/smart_start/jpa/smartstart/controller/UserController.java
+++ Back_End/smart-start/src/main/java/com/smart_start/jpa/smartstart/controller/UserController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,11 @@ public class UserController {
 	@GetMapping("/all")
 	public List<Users> getAll(){
 		return usersRepository.findAll();
+	}
+	
+	@PostMapping("/add")
+	public Users addUser(Users user) {
+		usersRepository.save(user);
+		return user;
 	}
 }
