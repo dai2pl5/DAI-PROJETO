e91568c51b06c9964a3c3cee15a06a1ce7f73fef
smartstartgsi <48456976+smartstartgsi@users.noreply.github.com>
2019-04-10 16:14:39 +0100

    Add files via upload

---
+++ Back_End/Smart_Start/src/main/java/com/boot/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.boot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.boot.model.User;
+
+@Repository("userRepository")
+public interface UserRepository extends JpaRepository<User, Long> {
+ 
+ User findByEmail(String email);
+}
