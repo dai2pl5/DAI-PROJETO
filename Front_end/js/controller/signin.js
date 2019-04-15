b7d0abf9e919ca326c59d0518b185db96e25cc88
Eduardo Gomes <teamdai19@gmail.com>
2019-04-15 20:55:16 +0100

    fetchs a funcionar

---
+++ Front_end/js/controller/signin.js
@@ -0,0 +1,30 @@
+function signin(){
+    console.log('signin() foi corrido');
+    var usernameOrEmail = document.getElementById('emailLogin').value
+    var password = document.getElementById('passwordLogin').value
+    
+    var data = {
+                usernameOrEmail:usernameOrEmail,
+                password:password
+        };
+        console.log(data);
+        fetch('http://localhost:8080/api/auth/signin',{
+        headers: {'Content-Type': 'application/json'},
+        credentials: "include",
+        method: 'POST',
+        body: JSON.stringify(data)
+        }).then(function(response){
+            if (!response.ok) {
+               alert('Deu merda');
+            } else {
+                alert("submitted with success");
+                console.log(response)
+                //window.location.href = "";
+                }
+        }).then(function (result) {
+            console.log(result);
+        }).catch(function (err) {
+            console.log(err);
+        });
+    }
+    
