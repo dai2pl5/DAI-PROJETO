ec824738cfe95d059237673b8659c94f4518ab69
jmelo <jpedrommelo@gmail.com>
2019-04-18 16:56:26 +0100

    dfghjk

---
+++ notika-master/notika/green-horizotal/selects.js
@@ -0,0 +1,18 @@
+window.onload = tipoHab();
+function tipoHab() {
+  document.getElementById("tipo_habitacao").value = "Apartamento"
+}
+window.onload = coberturas();
+function coberturas() {
+    //document.getElementById("paredes").setAttribute("selected", "false")
+    //document.getElementById("furto").setAttribute("selected", "selected")
+    //document.getElementById("tsunami").setAttribute("selected", "selected")
+}
+function edit() {
+    document.getElementById("name").disabled = false
+    document.getElementById("local").disabled =  false
+    document.getElementById("mail").disabled = false
+    document.getElementById("data").disabled = false
+    document.getElementById("tipo_habitacao").disabled = false
+    document.getElementById("coberturas").disabled = false
+}
