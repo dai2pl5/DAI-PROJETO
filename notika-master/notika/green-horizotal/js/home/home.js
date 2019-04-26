7df4eb502567201fb56169a9bcc603b33dff87bc
Eduardo <teamdai19@gmail.com>
2019-04-26 15:14:05 +0100

    asd

--- notika-master/notika/green-horizotal/js/home/home.js
+++ notika-master/notika/green-horizotal/js/home/home.js
@@ -4,7 +4,7 @@ window.onload = function(){
 
 function refresh() {
     async function fetchAsync() {
-        const render = document.getElementById("housesTable");
+        const render = document.getElementById("housesTableBody");
         let txt = "";
         const response = await fetch('http://localhost:8080/api/user/getHouses/',{
             headers: {'Content-Type': 'application/json'},
@@ -15,9 +15,10 @@ function refresh() {
         console.log(houses);
         //percorrer a variável users e por cada user cria a linha da tabela com os dados presentes
         for (const house of houses) {
-            txt += "<tr><td>" + house.idHome + "</td>" + "<td>" + house.morada + "</td>" + "<td> <button style='float: right;' class='btn btn-primary'><i class='glyphicon glyphicon-trash'></i></button> <button style='float: right' class='btn btn-primary'><i class='glyphicon glyphicon-edit'></i></button> </td></tr>"
+            txt += "<tr><td>" + house.idHome + "</td>" + "<td>" + house.morada + "</td>" + "<td> <button style='float: right;' class='btn btn-primary'><i class='glyphicon glyphicon-trash'></i></button> <button type = 'button' style='float: right' onclick='passContentDelete()'class='btn btn-primary'><i class='glyphicon glyphicon-edit'></i></button> </td></tr>"
         }
-        txt += "</tbody></table>";
+        
+    
         //envia a tabela construida para a view e mostra no object com ID result
         render.innerHTML = txt;
     }
@@ -30,7 +31,33 @@ function refresh() {
 }
 
 
-function deleteHouse(){
-
+function deleteHome(id){
     
+    console.log('delete a correr');
+            console.log(id);
+    fetch('http://localhost:8080/api/user/deleteHouse/' + id,{
+                headers: {'Content-Type': 'application/json'},
+                method: 'DELETE',
+                credentials : "include"
+            }).then(response =>
+                response.json().then(json => {
+                refresh();
+                return json;
+                
+    }));
+}
+    
+function passContentDelete(){
+    var id;
+    var table = document.getElementById('housesTable');
+    console.log
+    for(var i = 0; i<table.rows.length; i++){
+        table.rows[i].onclick = function(){
+            id = this.cells[0].innerHTML;
+            document.getElementById('id').value = id;
+            console.log(id);
+            deleteHome(id);
+            
+        }
+    }
 }
