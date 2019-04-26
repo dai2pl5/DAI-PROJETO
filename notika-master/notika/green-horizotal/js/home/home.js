8b1e3bb6ebdab1be9c2d0645a2e828d9e4ef3791
Eduardo <teamdai19@gmail.com>
2019-04-26 14:55:29 +0100

    aparecer casas de um user

---
+++ notika-master/notika/green-horizotal/js/home/home.js
@@ -0,0 +1,30 @@
+window.onload = function(){
+    refresh()
+}
+
+function refresh() {
+    async function fetchAsync() {
+        const render = document.getElementById("housesTable");
+        let txt = "";
+        const response = await fetch('http://localhost:8080/api/user/getHouses/',{
+            headers: {'Content-Type': 'application/json'},
+            credentials: "include",
+            method: 'GET'
+            });
+        const houses = await response.json();
+        console.log(houses);
+        //percorrer a variável users e por cada user cria a linha da tabela com os dados presentes
+        for (const house of houses) {
+            txt += "<tr><td>" + house.idHome + "</td>" + "<td>" + house.morada + "</td>" + "<td> <button style='float: right' class='btn btn-primary'><i class='glyphicon glyphicon-trash'></i></button> <button style='float: right' class='btn btn-primary'><i class='glyphicon glyphicon-edit'></i></button> </td></tr>"
+        }
+        txt += "</tbody></table>";
+        //envia a tabela construida para a view e mostra no object com ID result
+        render.innerHTML = txt;
+    }
+        //chama a função fetchAsync()
+        fetchAsync().then(function(data){
+        console.log('ok');
+        }).catch(function(reason){
+            console.log(reason.message);
+        });
+}
